45ad2ad4501c69e9de6a1a477d27e827a95e52a4
Ander-bel-00 <132407407+Ander-bel-00@users.noreply.github.com>
2024-02-08 08:54:08 -0500

    Interfaces

---
+++ backend/interfaces/admin final/js/main.js
@@ -0,0 +1,319 @@
+
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal"
+        });
+        $('#sidebar, #content').toggleClass('active');
+            logo.style.display = 'none';
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+
+            
+
+        // Selecciona el elemento del logo
+        var logo = $('#logo');
+
+        // Verifica si el menú está cerrado
+        if ($('#sidebar').hasClass('active')) {
+            // Si el menú está abierto, elimina la clase para mostrar el logo
+            logo.removeClass('hidden-logo');
+        } else {
+            // Si el menú está cerrado, agrega la clase para ocultar el logo
+            logo.addClass('hidden-logo');
+        }
+            
+   });
+   
+   
+});
+
+
+
+// Obtener referencias a los elementos HTML
+const cerrarSesionButton = document.getElementById('cerrarSesionButton'); // Botón "Cerrar sesión"
+const confirmacionCierreSesion = document.getElementById('confirmacionCierreSesion'); // Ventana de confirmación
+const confirmarCierreSesion = document.getElementById('confirmarCierreSesion'); // Botón "Sí, cerrar sesión"
+const cancelarCierreSesion = document.getElementById('cancelarCierreSesion'); // Botón "Cancelar"
+
+// Analizar cuando se le de click
+cerrarSesionButton.addEventListener('click', () => {
+    // Mostrar la ventana de confirmación al hacer clic en Cerrar sesión
+    confirmacionCierreSesion.style.display = 'block';
+});
+
+// Escucha cuando se le da click a "Sí, cerrar sesión"
+confirmarCierreSesion.addEventListener('click', () => {
+    
+    // Registrar un mensaje en la consola para demostración
+    console.log('Sesión cerrada');
+
+    // Ocultar la ventana de confirmación después de confirmar
+    confirmacionCierreSesion.style.display = 'none';
+});
+
+// Escuchar el evento de clic en el botón "Cancelar"
+cancelarCierreSesion.addEventListener('click', () => {
+    // Ocultar la ventana de confirmación al hacer clic en Cancelar
+    confirmacionCierreSesion.style.display = 'none';
+});
+
+
+
+//script para calendario
+
+let monthNames = ['Enero', 'Febreo', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let monthNumber = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+let dates = document.getElementById('dates');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+
+let prevMonthDOM = document.getElementById('prev-month');
+let nextMonthDOM = document.getElementById('next-month');
+
+month.textContent = monthNames[monthNumber];
+year.textContent = currentYear.toString();
+
+prevMonthDOM.addEventListener('click', ()=>lastMonth());
+nextMonthDOM.addEventListener('click', ()=>nextMonth());
+
+
+
+const writeMonth = (month) => {
+
+    for(let i = startDay(); i>0;i--){
+        dates.innerHTML += ` <div class="calendar__date calendar__item calendar__last-days">
+            ${getTotalDays(monthNumber-1)-(i-1)}
+        </div>`;
+    }
+
+    for(let i=1; i<=getTotalDays(month); i++){
+        if(i===currentDay) {
+            dates.innerHTML += ` <div class="calendar__date calendar__item calendar__today">${i}</div>`;
+        }else{
+            dates.innerHTML += ` <div class="calendar__date calendar__item">${i}</div>`;
+        }
+    }
+}
+
+const getTotalDays = month => {
+    if(month === -1) month = 11;
+
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+        return  31;
+
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+        return 30;
+
+    } else {
+
+        return isLeap() ? 29:28;
+    }
+}
+
+const isLeap = () => {
+    return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+const startDay = () => {
+    let start = new Date(currentYear, monthNumber, 1);
+    return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1;
+}
+
+const lastMonth = () => {
+    if(monthNumber !== 0){
+        monthNumber--;
+    }else{
+        monthNumber = 11;
+        currentYear--;
+    }
+
+    setNewDate();
+}
+
+const nextMonth = () => {
+    if(monthNumber !== 11){
+        monthNumber++;
+    }else{
+        monthNumber = 0;
+        currentYear++;
+    }
+
+    setNewDate();
+}
+
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}
+
+writeMonth(monthNumber);
+
+
+
+
+ /* subir archhivos */
+
+console.clear();
+('use strict');
+
+
+// Drag and drop - single or multiple image files
+// https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+// https://codepen.io/joezimjs/pen/yPWQbd?editors=1000
+(function () {
+
+  'use strict';
+  
+  // Four objects of interest: drop zones, input elements, gallery elements, and the files.
+  // dataRefs = {files: [image files], input: element ref, gallery: element ref}
+
+  const preventDefaults = event => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  const highlight = event =>
+    event.target.classList.add('highlight');
+  
+  const unhighlight = event =>
+    event.target.classList.remove('highlight');
+
+  const getInputAndGalleryRefs = element => {
+    const zone = element.closest('.upload_dropZone') || false;
+    const gallery = zone.querySelector('.upload_gallery') || false;
+    const input = zone.querySelector('input[type="file"]') || false;
+    return {input: input, gallery: gallery};
+  }
+
+  const handleDrop = event => {
+    const dataRefs = getInputAndGalleryRefs(event.target);
+    dataRefs.files = event.dataTransfer.files;
+    handleFiles(dataRefs);
+  }
+
+
+  const eventHandlers = zone => {
+
+    const dataRefs = getInputAndGalleryRefs(zone);
+    if (!dataRefs.input) return;
+
+    // Prevent default drag behaviors
+    ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(event => {
+      zone.addEventListener(event, preventDefaults, false);
+      document.body.addEventListener(event, preventDefaults, false);
+    });
+
+    // Highlighting drop area when item is dragged over it
+    ;['dragenter', 'dragover'].forEach(event => {
+      zone.addEventListener(event, highlight, false);
+    });
+    ;['dragleave', 'drop'].forEach(event => {
+      zone.addEventListener(event, unhighlight, false);
+    });
+
+    // Handle dropped files
+    zone.addEventListener('drop', handleDrop, false);
+
+    // Handle browse selected files
+    dataRefs.input.addEventListener('change', event => {
+      dataRefs.files = event.target.files;
+      handleFiles(dataRefs);
+    }, false);
+
+  }
+
+
+  // Initialise ALL dropzones
+  const dropZones = document.querySelectorAll('.upload_dropZone');
+  for (const zone of dropZones) {
+    eventHandlers(zone);
+  }
+
+
+  // No 'image/gif' or PDF or webp allowed here, but it's up to your use case.
+  // Double checks the input "accept" attribute
+  const isImageFile = file => 
+    ['image/jpeg', 'image/png', 'image/svg+xml'].includes(file.type);
+
+
+  function previewFiles(dataRefs) {
+    if (!dataRefs.gallery) return;
+    for (const file of dataRefs.files) {
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = function() {
+        let img = document.createElement('img');
+        img.className = 'upload_img mt-2';
+        img.setAttribute('alt', file.name);
+        img.src = reader.result;
+        dataRefs.gallery.appendChild(img);
+      }
+    }
+  }
+
+  // Based on: https://flaviocopes.com/how-to-upload-files-fetch/
+  const imageUpload = dataRefs => {
+
+    // Multiple source routes, so double check validity
+    if (!dataRefs.files || !dataRefs.input) return;
+
+    const url = dataRefs.input.getAttribute('data-post-url');
+    if (!url) return;
+
+    const name = dataRefs.input.getAttribute('data-post-name');
+    if (!name) return;
+
+    const formData = new FormData();
+    formData.append(name, dataRefs.files);
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('posted: ', data);
+      if (data.success === true) {
+        previewFiles(dataRefs);
+      } else {
+        console.log('URL: ', url, '  name: ', name)
+      }
+    })
+    .catch(error => {
+      console.error('errored: ', error);
+    });
+  }
+
+
+  // Handle both selected and dropped files
+  const handleFiles = dataRefs => {
+
+    let files = [...dataRefs.files];
+
+    // Remove unaccepted file types
+    files = files.filter(item => {
+      if (!isImageFile(item)) {
+        console.log('Not an image, ', item.type);
+      }
+      return isImageFile(item) ? item : null;
+    });
+
+    if (!files.length) return;
+    dataRefs.files = files;
+
+    previewFiles(dataRefs);
+    imageUpload(dataRefs);
+  }
+
+})();
