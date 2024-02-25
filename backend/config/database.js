ceb7e9bda0b90532e2f6795bbbaecbdfd2e3e1e9
Anderson <beltrananderson283@gmail.com>
2024-02-25 11:54:03 -0500

    Server

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,26 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new  Sequelize("seep","root","root",{
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Si la variable de entorno JAWSDB_URL está definida (como en Heroku), utiliza esa URL
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // De lo contrario, usa la configuración local
+  sequelize = new Sequelize("seep", "root", "root", {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
-
-async function testConnection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection established successfully');
-    }
-    catch(err){
-        console.error('Unable to connect to database', err);
-    }
+  });
 }
 
-module.exports = { sequelize, testConnection};
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection established successfully');
+  } catch (err) {
+    console.error('Unable to connect to database', err);
+  }
+}
+
+module.exports = { sequelize, testConnection };
