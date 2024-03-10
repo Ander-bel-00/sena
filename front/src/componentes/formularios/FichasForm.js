c77bcc022f4a018fc98f0df0c81de0da9dcfd3cd
cristiandavidcantillo <140770625+cristiandavidcantillo@users.noreply.github.com>
2024-03-10 16:17:08 -0500

    Add files via upload

---
+++ front/src/componentes/formularios/FichasForm.js
@@ -0,0 +1,80 @@
+import React, { Fragment, useState } from 'react';
+import clienteAxios from '../../api/axios';
+import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import './styles/Fichas.css';
+
+function NuevaFicha() {
+    
+    const [formData, setFormData] = useState({});
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value,});
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await clienteAxios.post('/fichas-add', formData);
+
+            Swal.fire({
+                title: 'Ficha registrada exitosamente',
+                icon: 'success',
+                showCancelButton: false,
+                confirmButtonText: 'Aceptar',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = '/';
+                }
+            });
+
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error al crear la ficha:', error);
+
+            if (error.response && error.response.data && error.response.data.mensaje) {
+                // Si hay un mensaje de error en la respuesta del servidor, mostrarlo
+                Swal.fire({
+                    title: 'Error',
+                    text: error.response.data.mensaje,
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar',
+                });
+            } else {
+                // Si no hay un mensaje de error específico, mostrar un mensaje genérico
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Hubo un error al crear la ficha',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar',
+                });
+            }
+        }
+    };
+
+    return (
+        <Fragment>
+            
+
+                <form className='formularioFichas' onSubmit={handleSubmit}>
+                    <h1 className='titulo-fichas'>Registro de fichas</h1>
+                    <input type="text" placeholder='Ingresa el número de ficha' name="numero_ficha"
+                        onChange={handleChange} required />
+                    <input type='text' placeholder='Programa de formación' name="programa_formacion"
+                        onChange={handleChange} required />
+                    <input type='text' placeholder='Nivel de formación' name='nivel_formacion'
+                        onChange={handleChange} required />
+                    <input type='text' placeholder='Titulo obtenido' required
+                        name='titulo_obtenido' onChange={handleChange} />
+                    <p>Fecha en que termina etapa lectiva: <input type='date' required
+                        name='fecha_fin_lectiva' onChange={handleChange} /></p>
+
+                    <button type="submit" className="btn btn-primary">Agregar nueva ficha</button>
+                </form>
+        
+        </Fragment>
+    )
+}
+
+export default NuevaFicha;
---
+++ front/src/componentes/instructores/Instructor.js
@@ -0,0 +1,83 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import clienteAxios from '../../api/axios';
+import logoSena from './sena-verde.png';
+import { Link } from 'react-router-dom';
+
+function Instructor() {
+  const [usuario, setUsuario] = useState(null);
+  const [fichasAsignadas, setFichasAsignadas] = useState([]);
+
+  useEffect(() => {
+    const obtenerUsuario = async () => {
+      try {
+        // Hacer la solicitud GET al endpoint '/usuario' para obtener la información del usuario autenticado
+        const response = await clienteAxios.get('/usuario');
+
+        // Establecer el usuario en el estado del componente
+        setUsuario(response.data.usuario);
+
+        // Obtener las fichas asignadas al instructor
+        const responseFichas = await clienteAxios.get(`/instructor/${response.data.usuario.numero_documento}/fichas-asignadas`);
+        // Ordenar Fichas desde la primera creada a la más reciente.
+        const fichasOrdenadas = responseFichas.data.fichasAsignadas.sort((a, b) => {
+          // Convertir las fechas de creación a objetos Date y compararlas
+          return new Date(a.createdAt) - new Date(b.createdAt);
+        });
+        setFichasAsignadas(fichasOrdenadas);
+      } catch (error) {
+        console.error('Error al obtener la información del usuario:', error);
+      }
+    };
+
+    obtenerUsuario();
+  }, []); // El array vacío como segundo argumento asegura que useEffect se ejecute solo una vez al montar el componente
+
+  // Función para formatear la fecha en el formato Año-mes-día
+  const formatearFecha = (fechaString) => {
+    const fecha = new Date(fechaString);
+    return fecha.toISOString().split('T')[0];
+  };
+
+  return (
+    <Fragment>
+      <div>
+        {usuario && usuario.rol_usuario ? (
+          <Fragment>
+            <h2 className='fichasAsignedTitle mt-11 text-center'>Agendamiento de Visitas</h2>
+            <h5 className='text-center text-gray-500 selctFicha'>Selecciona una ficha</h5>
+            <div className="row my-2 fichas-content rounded-md">
+              {fichasAsignadas.length > 0 ? (
+                <div className="fichas-grid">
+                  {fichasAsignadas.map(ficha => (
+                    <div key={ficha.numero_ficha} className="ficha-card">
+                      <div className="card fichas">
+                        <div className="card-body">
+                          <h5 className="card-title">Ficha {ficha.numero_ficha}</h5>
+                          <img src={logoSena} className='w-9 logSenaFichas relative'/>
+                          <p className="card-text"><strong>Programa de formación: </strong>{ficha.programa_formacion}</p>
+                          <p className="card-text"><strong>Nivel de formación: </strong>{ficha.nivel_formacion}</p>
+                          <p className="card-text"><strong>Título obtenido: </strong>{ficha.titulo_obtenido}</p>
+                          <p className="card-text"><strong>Fecha fin lectiva: </strong>{formatearFecha(ficha.fecha_fin_lectiva)}</p>
+                        </div>
+                        <button className='btnVerFicha'>
+                          <Link to={`/${usuario.rol_usuario}/aprendicesFicha/${ficha.numero_ficha}`} className='verFichas'>
+                            Ver Ficha</Link>
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p>No hay fichas asignadas</p>
+              )}
+            </div>
+          </Fragment>
+        ) : (
+          <p>Cargando usuario...</p>
+        )}
+      </div>
+    </Fragment>
+  );
+}
+
+export default Instructor;
