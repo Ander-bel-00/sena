c6c1718850fff8b31a9b782e283716542748a7de
Anderson <beltrananderson283@gmail.com>
2024-03-18 16:41:37 -0500

    Se ha agregado el panel del administrador, por favor realizar mejoras para el diseño del panel y vista responsive, se ha agregado plantilla para el correo y se configuró motor de plantilals para hacer las que sean necesarias

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from 'js-cookie';
 
 const clienteAxios = axios.create({
-    baseURL: 'http://192.168.1.25:5000',
+    baseURL: 'http://10.200.81.26:5000',
     withCredentials: true,
 });
 
