456bc76e0ae11c0e415df6580a692dcdde2dfb8a
Anderson <beltrananderson283@gmail.com>
2024-03-18 22:12:44 -0500

    Se ha agregado el panel del administrador, por favor realizar mejoras para el diseño del panel y vista responsive, se ha agregado plantilla para el correo y se configuró motor de plantilals para hacer las que sean necesarias

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
 
-
-const sequelize = new Sequelize(process.env.database || "seep", 
-process.env.user || "root", process.env.password || "root", {
-  host: process.env.host || "localhost",
-  dialect: process.env.DIALECT_DB || 'mysql',
-  port: process.env.port|| 3306
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "seep", 
+  process.env.USER_DB || "Ander",
+  process.env.PASSWORD_DB || "Ab%1234567", {
+    host: process.env.HOST_NAME || "SG-bitter-cobweb-9346-8256-mysql-master.servers.mongodirector.com",
+    dialect: process.env.DIALECT_DB || 'mysql',
+    port: process.env.PORTDB || 3306
 });
 
 async function testConnection() {
