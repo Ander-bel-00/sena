a6a2edc7a07a20dc5ee9c65f616e7571dea35088
cristiandavidcantillo <140770625+cristiandavidcantillo@users.noreply.github.com>
2024-03-19 08:51:12 -0500

    Update FooterComponent.js

--- frontend/src/componentes/admin/layouts/FooterComponent.js
+++ frontend/src/componentes/admin/layouts/FooterComponent.js
@@ -7,16 +7,10 @@ function Footer() {
       <div className="footer-content">
         <div className="footer-section">
           <h2>Todos los derechos reservados</h2>
-          <p>Teléfono: 3008345320</p>
         </div>
 
         <div className="footer-section">
           <h2>@SENA</h2>
-          <ul>
-            <li><a href="/about"></a></li>
-            <li><a href="/services"></a></li>
-            <li><a href="/contact">Contcto</a></li>
-          </ul>
         </div>
 
         
