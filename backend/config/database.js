2daa480c9f5d27af343a77b3a81199927c6d6fc0
Anderson <beltrananderson283@gmail.com>
2024-03-18 21:30:17 -0500

    Se ha agregado el panel del administrador, por favor realizar mejoras para el diseño del panel y vista responsive, se ha agregado plantilla para el correo y se configuró motor de plantilals para hacer las que sean necesarias

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-
 const sequelize = new Sequelize(process.env.database || "seep", 
-process.env.user || "root", process.env.password || "root", {
-  host: process.env.host || "localhost",
-  dialect: process.env.DIALECT_DB || 'mysql',
-  port: process.env.port|| 3306
+    process.env.user || "Ander", process.env.password || "Ab%1234567", {
+    host: process.env.host || "SG-bitter-cobweb-9346-8256-mysql-master.servers.mongodirector.com",
+    dialect: process.env.DIALECT_DB || 'mysql',
+    port: process.env.port || 3306
 });
 
 async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection established successfully');
-  } catch (err) {
-    console.error('Unable to connect to database', err);
-  }
+    try {
+        await sequelize.authenticate();
+        console.log('Connection established successfully');
+    } catch (err) {
+        console.error('Unable to connect to database', err);
+    }
 }
 
 module.exports = { sequelize, testConnection };
